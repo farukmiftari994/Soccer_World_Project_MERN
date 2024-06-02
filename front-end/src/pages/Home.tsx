2c2f6314137ad4f82e9f035f87774a3dd72a060d
Faruk Miftari <farukmiftari994@gmail.com>
2024-06-02 02:08:48 +0200

    "Added proxy to back-end package.json and modified Home.tsx to include API request with headers and error handling"

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,16 +1,34 @@
 import "../App.css";
-import image from "../img/Presentation1.png";
+
+const headers = new Headers();
+headers.append("X-Auth-Token", "e833bfee20e24972a8c19d762ee25968");
+
+const requestOptions: RequestInit = {
+  method: "GET",
+  headers: headers,
+};
+
+try {
+  fetch(
+    "https://api.football-data.org/v4/competitions/DED/standings",
+    requestOptions
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.error("Error:", error));
+} catch (error) {
+  console.error("Fetch error:", error);
+}
 
 function Home() {
   return (
     <>
-      <div className="content-container">
-        <img
-          src={image}
-          alt=""
-          style={{ marginTop: "120px", width: "80%", height: "70%" }}
-        />
-      </div>
+      <div className="content-container"></div>
     </>
   );
 }
