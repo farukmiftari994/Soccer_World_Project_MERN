65b8a580f03c779f305c24991fb2d65f4992fb9b
Faruk Miftari <farukmiftari994@gmail.com>
2024-06-06 12:12:08 +0200

    Removed API call and headers configuration from Home component

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,33 +1,9 @@
 import "../App.css";
 
-async function Home() {
-  const myHeaders = new Headers();
-  myHeaders.append("X-Auth-Token", "e833bfee20e24972a8c19d762ee25968");
-
-  const requestOptions: RequestInit = {
-    method: "GET",
-    headers: myHeaders,
-    mode: "no-cors",
-  };
-  try {
-    const response = await fetch(
-      "https://api.football-data.org/v4/competitions/DED/standings",
-      requestOptions
-    );
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    } else {
-      const result = await response.json();
-      console.log("result :>> ", result);
-    }
-  } catch (error) {
-    console.log("error :>> ", error);
-  }
-
+function Home() {
   return (
     <div className="content-container">
       <h1>Football Standings</h1>
-      {/* Render standings here */}
     </div>
   );
 }
